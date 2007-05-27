0ddd94d00ffb678dea9d7f7e5ab3c02d1dc38012
denis.defreyne <denis.defreyne@e6c0311f-002e-0410-bcfe-736e5f403364>
2007-05-27 08:42:05 +0000

    added some string parsing helpers

---
+++ trunk/NSString+Parsing.h
@@ -0,0 +1,15 @@
+//
+//	NSString+Parsing.h
+//	ScrobScrob
+//
+//	Created by Denis Defreyne on 12/04/2007.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSString (Parsing)
+
+- (BOOL)beginsWith:(NSString *)aString;
+
+@end
